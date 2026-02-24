e24c73ccd5832325787031ac48e8416219009335
bowang <bbw1996@qq.com>
2026-02-24 14:50:10 +0800

    update

--- dist/resume-of-mine.59712999.js
+++ dist/resume-of-mine.59712999.js
@@ -207,11 +207,11 @@
       });
     }
   }
-})({"1v1Pl":[function(require,module,exports,__globalThis) {
+})({"2HynS":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 1234;
+var HMR_SERVER_PORT = 3000;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -715,6 +715,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"4M6V8":[function(require,module,exports,__globalThis) {
 
-},{}]},["1v1Pl","4M6V8"], "4M6V8", "parcelRequire94c2", {})
+},{}]},["2HynS","4M6V8"], "4M6V8", "parcelRequire94c2", {})
 
 //# sourceMappingURL=resume-of-mine.59712999.js.map
